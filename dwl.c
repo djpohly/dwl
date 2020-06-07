8c0afa7c738752c18970209365fd289b1005008a
Devin J. Pohly <djpohly@gmail.com>
2020-06-07 01:11:46 -0500

    send xdg_decoration configure right after create
    Thanks to @cedws for pointing this out!

--- dwl.c
+++ dwl.c
@@ -500,13 +500,14 @@ void
 createxdeco(struct wl_listener *listener, void *data)
 {
 	struct wlr_xdg_toplevel_decoration_v1 *wlr_deco = data;
-	Decoration *d;
+	Decoration *d = wlr_deco->data = calloc(1, sizeof(*d));
 
-	d = wlr_deco->data = calloc(1, sizeof(*d));
 	wl_signal_add(&wlr_deco->events.request_mode, &d->request_mode);
 	d->request_mode.notify = getxdecomode;
 	wl_signal_add(&wlr_deco->events.destroy, &d->destroy);
 	d->destroy.notify = destroyxdeco;
+
+	getxdecomode(&d->request_mode, wlr_deco);
 }
 
 
