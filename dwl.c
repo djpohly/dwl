68412d8957ea31f92014dfc8813a1f294a55211b
Guido Cella <guidocella91@gmail.com>
2020-09-08 12:49:05 +0200

    try to fix again
    Calculate x and y of usable_area, not just width and heigth.

--- dwl.c
+++ dwl.c
@@ -550,7 +550,7 @@ arrangelayer(Monitor *m, struct wl_list *list, struct wlr_box *usable_area, bool
 void
 arrangelayers(Monitor *m)
 {
-	struct wlr_box usable_area = { 0 };
+	struct wlr_box usable_area = *wlr_output_layout_get_box(output_layout, m->wlr_output);
 	uint32_t layers_above_shell[] = {
 		ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY,
 		ZWLR_LAYER_SHELL_V1_LAYER_TOP,
@@ -558,9 +558,6 @@ arrangelayers(Monitor *m)
 	size_t nlayers = LENGTH(layers_above_shell);
 	LayerSurface *layersurface;
 	struct wlr_keyboard *kb = wlr_seat_get_keyboard(seat);
-
-	wlr_output_effective_resolution(m->wlr_output,
-			&usable_area.width, &usable_area.height);
 
 	// Arrange exclusive surfaces from top->bottom
 	arrangelayer(m, &m->layers[ZWLR_LAYER_SHELL_V1_LAYER_OVERLAY],
