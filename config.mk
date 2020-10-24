ff416e7da86b96ad4de20a3b1df4b8ef7897e6ce
Vinko Kašljević <vinko.kasljevic@gmail.com>
2020-10-24 19:57:28 +0200

    Move from Makefile to Meson build system

--- config.mk
+++
@@ -1,5 +0,0 @@
-# Default compile flags (overridable by environment)
-CFLAGS ?= -g -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-error=unused-function
-
-# Uncomment to build XWayland support
-#CFLAGS += -DXWAYLAND
