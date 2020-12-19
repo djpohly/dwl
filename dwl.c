ba1540c3d006eb001e45e3b9315a00c9c599d2cc
Guido Cella <guido@guidocella.xyz>
2020-12-19 19:32:16 +0100

    remove goto when the border is 0
    Rendering 0-dimension rectangles no longer crashes wlroots.

--- dwl.c
+++ dwl.c
@@ -1721,8 +1721,6 @@ renderclients(Monitor *m, struct timespec *now)
 		ox = c->geom.x, oy = c->geom.y;
 		wlr_output_layout_output_coords(output_layout, m->wlr_output,
 				&ox, &oy);
-		if (c->bw == 0)
-			goto render;
 
 		w = surface->current.width;
 		h = surface->current.height;
@@ -1741,7 +1739,6 @@ renderclients(Monitor *m, struct timespec *now)
 					m->wlr_output->transform_matrix);
 		}
 
-render:
 		/* This calls our render function for each surface among the
 		 * xdg_surface's toplevel and popups. */
 		rdata.output = m->wlr_output;
