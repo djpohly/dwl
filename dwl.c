33c36be2fc7df3815abfcd8e0bc0bc2b65554ec5
Devin J. Pohly <djpohly@gmail.com>
2020-12-28 15:51:04 -0500

    Revert "Allow toggling the layout before selecting a different one"
    This reverts commit 90cc3b1e2c824db74e932dbb9733d398619a037c.  Didn't
    mean to merge this change.

--- dwl.c
+++ dwl.c
@@ -837,8 +837,7 @@ createmon(struct wl_listener *listener, void *data)
 			m->nmaster = r->nmaster;
 			wlr_output_set_scale(wlr_output, r->scale);
 			wlr_xcursor_manager_load(cursor_mgr, r->scale);
-			m->lt[0] = r->lt;
-			m->lt[1] = &layouts[LENGTH(layouts) > 1 && r->lt != &layouts[1]];
+			m->lt[0] = m->lt[1] = r->lt;
 			wlr_output_set_transform(wlr_output, r->rr);
 			m->position = r - monrules;
 			break;
