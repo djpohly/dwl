0db6f3c5b5f971bebfbbf8ec9bf94134f5506600
Humm <hummsmith42@gmail.com>
2022-01-05 01:54:02 +0100

    add dwl(1)
    Documentation is good.  Man pages are documentation.  A program without
    a man page is worthless.

--- config.mk
+++ config.mk
@@ -1,5 +1,6 @@
 # paths
 PREFIX = /usr/local
+MANDIR = $(PREFIX)/share/man
 
 # Default compile flags (overridable by environment)
 CFLAGS ?= -g -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wno-unused-result -Wdeclaration-after-statement
