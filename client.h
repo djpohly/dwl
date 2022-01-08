f587b2fd2c9f5967b9e8ca99fe70fbc80fb6c220
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2022-01-08 17:41:45 +0100

    fix client_set_tiled, which was ignoring its "edges" argument

--- client.h
+++ client.h
@@ -156,8 +156,7 @@ client_set_tiled(Client *c, uint32_t edges)
 	if (client_is_x11(c))
 		return;
 #endif
-	wlr_xdg_toplevel_set_tiled(c->surface.xdg, WLR_EDGE_TOP |
-			WLR_EDGE_BOTTOM | WLR_EDGE_LEFT | WLR_EDGE_RIGHT);
+	wlr_xdg_toplevel_set_tiled(c->surface.xdg, edges);
 }
 
 static inline struct wlr_surface *
