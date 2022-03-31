b08dd2187d328ec1aacd988202efb066b7bc805d
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-03-31 09:21:27 -0600

    don't let -pedantic be overridable by environment

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ PREFIX = /usr/local
 MANDIR = $(PREFIX)/share/man
 
 # Default compile flags (overridable by environment)
-CFLAGS ?= -g -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wno-unused-result -Wdeclaration-after-statement -pedantic
+CFLAGS ?= -g -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wno-unused-result -Wdeclaration-after-statement
 
 # Uncomment to build XWayland support
 #CFLAGS += -DXWAYLAND
