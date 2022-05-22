283c043b5c8cc6f2d5b8a5e72e1a5924b2c1532e
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-05-21 20:44:08 -0500

    chase wlroots scene-surface refactor

--- dwl.c
+++ dwl.c
@@ -2253,8 +2253,9 @@ xytonode(double x, double y, struct wlr_surface **psurface,
 
 	for (layer = focus_order; layer < END(focus_order); layer++) {
 		if ((node = wlr_scene_node_at(layers[*layer], x, y, nx, ny))) {
-			if (node->type == WLR_SCENE_NODE_SURFACE)
-				surface = wlr_scene_surface_from_node(node)->surface;
+			if (node->type == WLR_SCENE_NODE_BUFFER)
+				surface = wlr_scene_surface_from_buffer(
+						wlr_scene_buffer_from_node(node))->surface;
 			/* Walk the tree to find a node that knows the client */
 			for (pnode = node; pnode && !c; pnode = pnode->parent)
 				c = pnode->data;
