c70db2d06a7c868ba2d36de0984f5b39afd1a1c6
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-07-19 11:52:15 -0500

    Revert "only call wlr_seat_keyboard_notify_enter() if a keyboard is found"
    This reverts commit 8e03bce6217117f0687cd727ae2c47bdd3c0fe5a.
    fixes #270

--- dwl.c
+++ dwl.c
@@ -613,6 +613,8 @@ arrangelayers(Monitor *m)
 				if (kb)
 					wlr_seat_keyboard_notify_enter(seat, layersurface->layer_surface->surface,
 							kb->keycodes, kb->num_keycodes, &kb->modifiers);
+				else
+					wlr_seat_keyboard_notify_enter(seat, layersurface->layer_surface->surface, NULL, 0, NULL);
 				return;
 			}
 		}
@@ -1183,6 +1185,8 @@ focusclient(Client *c, int lift)
 	if (kb)
 		wlr_seat_keyboard_notify_enter(seat, client_surface(c),
 				kb->keycodes, kb->num_keycodes, &kb->modifiers);
+	else
+		wlr_seat_keyboard_notify_enter(seat, client_surface(c), NULL, 0, NULL);
 
 	/* Activate the new client */
 	client_activate_surface(client_surface(c), 1);
