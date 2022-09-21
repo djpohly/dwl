31106eff64406a004beb18c265a8b6fa0ae4f7a5
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-09-20 23:34:48 -0500

    rework Makefile
    - allow user to use a different pkg-config binary
    - restore almost all (and add other ones) warning flags (-Werror is not set)
    - $(XWAYLAND) is added to our CPPFLAGS
    - remove useless comments
    - don't generate idle-protocol.h (not used)

--- config.mk
+++ config.mk
@@ -1,12 +1,11 @@
 _VERSION = 0.3.1-dev
 VERSION  = `git describe --long --tags --dirty 2>/dev/null || echo $(_VERSION)`
 
+PKG_CONFIG = pkg-config
+
 # paths
 PREFIX = /usr/local
 MANDIR = $(PREFIX)/share/man
-
-# Compile flags that can be used
-#CFLAGS = -pedantic -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wno-unused-result -Wdeclaration-after-statement
 
 XWAYLAND =
 XLIBS =
