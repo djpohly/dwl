c91d21b68f436bc61dfce8b3f47beb5855bdd1a2
Leonardo Hernández Hernández <leohdz172@protonmail.com>
2022-11-25 12:15:55 -0600

    do not move/resize if grabbed client is fullscreen

--- dwl.c
+++ dwl.c
@@ -1510,7 +1510,7 @@ moveresize(const Arg *arg)
 	if (cursor_mode != CurNormal && cursor_mode != CurPressed)
 		return;
 	xytonode(cursor->x, cursor->y, NULL, &grabc, NULL, NULL, NULL);
-	if (!grabc || client_is_unmanaged(grabc))
+	if (!grabc || client_is_unmanaged(grabc) || grabc->isfullscreen)
 		return;
 
 	/* Float the window and tell motionnotify to grab it */
