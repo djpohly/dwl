e4921fad28081f36fa0daa61b4bef7022a21b340
gan-of-culture <53971163+gan-of-culture@users.noreply.github.com>
2023-02-25 22:59:23 +0100

    add missing comma in "MonitorRule"
    add a missing comma for the example

--- config.def.h
+++ config.def.h
@@ -31,7 +31,7 @@ static const Layout layouts[] = {
 static const MonitorRule monrules[] = {
 	/* name       mfact nmaster scale layout       rotate/reflect                x    y */
 	/* example of a HiDPI laptop monitor:
-	{ "eDP-1",    0.5,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL    -1,  -1 },
+	{ "eDP-1",    0.5,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 	*/
 	/* defaults */
 	{ NULL,       0.55, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
