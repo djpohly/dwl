c7ad2efa523005f5a61a9de06b9a8aa793f7d8c8
A Frederick Christensen <dwl@ivories.org>
2023-02-28 21:32:35 -0600

    Missing apostrophe

--- dwl.c
+++ dwl.c
@@ -424,7 +424,7 @@ applybounds(Client *c, struct wlr_box *bbox)
 		c->geom.width = MAX(min.width + (2 * (int)c->bw), c->geom.width);
 		c->geom.height = MAX(min.height + (2 * (int)c->bw), c->geom.height);
 		/* Some clients set their max size to INT_MAX, which does not violate the
-		 * protocol but its unnecesary, as they can set their max size to zero. */
+		 * protocol but it's unnecesary, as they can set their max size to zero. */
 		if (max.width > 0 && !(2 * c->bw > INT_MAX - max.width)) /* Checks for overflow */
 			c->geom.width = MIN(max.width + (2 * c->bw), c->geom.width);
 		if (max.height > 0 && !(2 * c->bw > INT_MAX - max.height)) /* Checks for overflow */
