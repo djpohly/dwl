7878d911d6675710cb52b89463b6c0724f665d5f
A Frederick Christensen <dwl@ivories.org>
2023-06-07 16:24:28 -0500

    Stray whitespace fixes

--- dwl.c
+++ dwl.c
@@ -1047,7 +1047,7 @@ createpointer(struct wlr_pointer *pointer)
 
 		if (libinput_device_config_scroll_get_methods(libinput_device) != LIBINPUT_CONFIG_SCROLL_NO_SCROLL)
 			libinput_device_config_scroll_set_method (libinput_device, scroll_method);
-		
+
 		if (libinput_device_config_click_get_methods(libinput_device) != LIBINPUT_CONFIG_CLICK_METHOD_NONE)
 			libinput_device_config_click_set_method (libinput_device, click_method);
 
