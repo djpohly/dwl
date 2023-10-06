d63f4078c51c484a47b1c463912dc6038e787426
Leonardo Hernández Hernández <leohdz172@proton.me>
2023-10-05 22:09:08 -0600

    use the same border colors as dwm

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@
 static const int sloppyfocus               = 1;  /* focus follows mouse */
 static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will disable idle tracking even if it's surface isn't visible  */
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
-static const float bordercolor[]           = COLOR(0x808080ff);
-static const float focuscolor[]            = COLOR(0xff0000ff);
+static const float bordercolor[]           = COLOR(0x444444ff);
+static const float focuscolor[]            = COLOR(0x005577ff);
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0}; /* You can also use glsl colors */
 
