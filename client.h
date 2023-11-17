7bdbab04000c23638afeb8dbeddd628c4f15117c
Leonardo Hernández Hernández <leohdz172@proton.me>
2023-11-16 21:48:56 -0600

    check toplevel resource instead of client's xdg_shell to set bounds

--- client.h
+++ client.h
@@ -111,7 +111,7 @@ client_set_bounds(Client *c, int32_t width, int32_t height)
 	if (client_is_x11(c))
 		return 0;
 #endif
-	if (c->surface.xdg->client->shell->version >=
+	if (wl_resource_get_version(c->surface.xdg->toplevel->resource) >=
 			XDG_TOPLEVEL_CONFIGURE_BOUNDS_SINCE_VERSION && width >= 0 && height >= 0)
 		return wlr_xdg_toplevel_set_bounds(c->surface.xdg->toplevel, width, height);
 	return 0;
